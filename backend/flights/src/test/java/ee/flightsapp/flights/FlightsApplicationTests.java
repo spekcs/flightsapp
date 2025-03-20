6dd7243c75242375fb47ff7bc72be746f6bf541a
Specks <speccer.fim@gmail.com>
2025-03-20 10:55:53 +0200

    Initialized with backend

---
+++ backend/flights/src/test/java/ee/flightsapp/flights/FlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.flightsapp.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
