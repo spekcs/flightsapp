d1e783e9fabf110c8415800cd3853d90d088a8bf
Specks <speccer.fim@gmail.com>
2025-03-27 17:48:55 +0200

    Added database

--- frontend/flightsapp/src/pages/Login.tsx
+++ frontend/flightsapp/src/pages/Login.tsx
@@ -1,7 +1,28 @@
+import LoginForm from "@/components/ui/loginform";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 function Login() {
-    return(
-        <p>Login</p>
-    );
+    function handleLogin(values: { username: string; password: string}) {
+        console.log(values)
+    }
+
+    return(<div className="h-screen flex justify-center items-center align-middle">
+        <Card className="w-[350]px">
+            <CardHeader>
+                <CardTitle>Login</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <LoginForm onSubmit={handleLogin} />
+            </CardContent>
+        </Card>
+        </div>
+    )
 }
 
 export default Login;
--- frontend/flightsapp/src/pages/Register.tsx
+++ frontend/flightsapp/src/pages/Register.tsx
@@ -1,73 +1,28 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod"
+import LoginForm from "@/components/ui/loginform";
 
-import { Button } from "@/components/ui/button"
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-
-import { Input } from "@/components/ui/input"
-
-const formSchema = z.object({
-    username: z.string().min(2).max(20),
-    password: z.string().min(8, { message: "Password must be at least 8 characters" })
-})
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function Register() {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            username: "",
-            password: ""
-        }
-    })
-
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    function handleRegister(values: { username: string; password: string}) {
         console.log(values)
     }
-    return (
-        <div className="h-screen flex justify-center items-center">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="John" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
 
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
+    return(<div className="h-screen flex justify-center items-center align-middle">
+        <Card className="w-[350]px">
+            <CardHeader>
+                <CardTitle>Register</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <LoginForm onSubmit={handleRegister} />
+            </CardContent>
+        </Card>
         </div>
-    );
+    )
 }
 
 export default Register;
